1c38e6ff5d4c5adbbd37d040a22a2f9fcf44a21c
Caleb Hicks <calebhicks@gmail.com>
2015-05-19 15:59:53 -0600

    adds project

---
+++ BrickStacker/BrickStacker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrickStacker
+//
+//  Created by Caleb Hicks on 5/19/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
